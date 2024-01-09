4db3718f386f06c9266c944b29fdf4ebaf2fa236
Eleni <elenimariex@gmail.com>
2024-01-09 23:15:43 +0000

    Including form in contact section

--- portfolio/src/components/Contact/Contact.jsx
+++ portfolio/src/components/Contact/Contact.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Header from '../Header/Header'
+import Form from './Form.jsx'
 import "./Contact.css"
 
 function Contact(){
@@ -12,6 +13,9 @@ function Contact(){
                 <a href="https://www.linkedin.com/in/eleni-giannopoulos/" target='_blank'><img src="./src/images/linkedin-logo-linkedin-icon-transparent-free-png.webp" alt="LinkedIn Logo" id="linkedin-logo"/></a>
                 <a href="https://github.com/EleniMG" target='_blank'><img src="./src/images/github-logo.png" alt="GitHub Logo" id="github-logo" /></a>
             </div>
+        </div>
+        <div className="form-section">
+            <Form />
         </div>
     </>
     )
--- portfolio/src/components/Contact/Form.jsx
+++ portfolio/src/components/Contact/Form.jsx
@@ -6,10 +6,78 @@ function Form() {
     firstName: '',
     lastName: '',
     emailAddress: '',
-    message: '',
+    message: ''
   });
 
- 
+  const handleInputChange = (event) => {
+    // Getting the value and name of the input which triggered the change
+    let value = event.target.value;
+    const name = event.target.name;
+
+    // Updating the input's state
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = (event) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    event.preventDefault();
+    if (!formData.firstName || !formData.lastName) {
+      alert('Fill out your first and last name please!');
+    } else if (/(\w+)@(\w+)\.(\w+)/gi.test(formData.email) === false) {
+      alert("Please enter a valid email address");
+    } else {
+      alert(`Hello ${formData.firstName} ${formData.lastName}, I'm grateful for you reaching out. However, your message has not been received. Nothing personal, it's just that this is a personal project... But feel free to reach out to me on LinkedIn! 😊`);
+    }
+
+    setFormData({
+      firstName: '',
+      lastName: '',
+      emailAddress: '',
+      message: ''
+    });
+  };
+
+  return (
+    <div>
+      <p>
+        Hello {formData.firstName} {formData.lastName}
+      </p>
+      <form className="form">
+        <input
+          value={formData.firstName}
+          name="firstName"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="First Name"
+        />
+        <input
+          value={formData.lastName}
+          name="lastName"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Last Name"
+        />
+        <input
+          value={formData.email}
+          name="email"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Email"
+        />
+        <input
+          value={formData.message}
+          name="message"
+          onChange={handleInputChange}
+          type="text"
+          placeholder="Message"
+        />
+        <button onClick={handleFormSubmit}>Submit</button>
+      </form>
+    </div>
+  );
 }
 
 export default Form;
